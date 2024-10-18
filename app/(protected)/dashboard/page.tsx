b6750dc4796ccfc7de91b9eeefeb03e71f4f940a
miluo16 <miluo@udel.edu>
2024-10-18 17:14:06 -0400

    reconstruct code

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -3,15 +3,25 @@ import Link from "next/link";
 
 const DashboardPage = async () => {
   const session = await auth();
+<<<<<<< Updated upstream
   return (
     <section className="main-container">
+=======
+  return (    
+<section className="main-container">
+>>>>>>> Stashed changes
       <h1 className="header-text text-3xl font-bold mt-4">
         This is employee dashboard Page
       </h1>
       <p className="mt-4 text-lg">
         Current User email: {session?.user?.email || "None"}
+<<<<<<< Updated upstream
       </p>
     </section>
+=======
+      </p>    </section>
+
+>>>>>>> Stashed changes
   );
 };
 
